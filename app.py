d2b53fa3ef439b0ce67fe1eb548f7eb435d275e9
hadi <aalgvs8@gmail.com>
2026-02-10 19:12:17 +0330

    init01

---
+++ app.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+from module import School
+
+data = {
+    "name": ["Ali", "Sara", "Reza", "Mina", "Hadi", "Nima", "Zahra"],
+    "math": [18, 15, np.nan, 20, 17, 14, 19],
+    "physics": [17, np.nan, 16, 19, 18, 13, 20],
+    "chemistry": [19, 20, 1, 4, 9, np.nan, np.nan],
+    "class": ["A", "A", "B", "B", "A", "B", "A"]
+}
+df = pd.DataFrame(data)
+ins = School(df)
+while True:
+    print('''
+          1-replace nan fields wiht mean of course
+          2-add avg column
+          3-top student in courses
+          4-courses mean
+          5-min score
+          6-max score
+          7-export to csv
+          or exit
+          ''')
+    opt = input('choose option: ')
+    
+    if opt == '1':
+        print(ins.replace_nan_instead_mean())
+        
+    elif opt == '2':
+        print(ins.add_avg_column())
+        
+    elif opt == '3':
+        print('''
+              1-math
+              2-chemistry
+              3-physics
+              ''')
+        ts_opt = input('enter course: ')
+        if ts_opt == '1':
+            print(ins.top_student_math())
+        elif ts_opt == '2':
+            print(ins.top_student_chemistry())
+        elif ts_opt == '3':
+            print(ins.top_student_physics())
+        else:
+            print('invalid')
+            
+    elif opt == '4':
+        print(ins.course_mean())
+        
+    elif opt == '5':
+        print(ins.min_score())
+    
+    elif opt =='6':
+        print(ins.max_score())
+        
+    elif opt =='7':
+        new_df = ins.get_dataframe()
+        new_df.to_csv('school.csv', index=False)
+        
+    elif opt == 'exit':
+        break
+    
+    else:
+        print('invalid')
---
+++ module.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+
+class School:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        
+    def replace_nan_instead_mean(self):
+        self.dataframe['math'] = self.dataframe['math'].fillna(self.dataframe['math'].mean())
+        self.dataframe['physics'] = self.dataframe['physics'].fillna(self.dataframe['physics'].mean())
+        self.dataframe['chemistry'] = self.dataframe['chemistry'].fillna(self.dataframe['chemistry'].mean())
+        return self.dataframe
+    
+    def add_avg_column(self):
+        self.dataframe['average'] = self.dataframe[['math', 'physics', 'chemistry']].mean(axis=1)
+        return self.dataframe
+    
+    def top_student_math(self):
+        student = self.dataframe.loc[self.dataframe['math'].idxmax()]
+        return student
+    
+    def top_student_physics(self):
+        student = self.dataframe.loc[self.dataframe['physics'].idxmax()]
+        return student
+    
+    def top_student_chemistry(self):
+        student = self.dataframe.loc[self.dataframe['chemistry'].idxmax()]
+        return student
+    
+    def course_mean(self):
+        math = self.dataframe['math'].mean()
+        physics = self.dataframe['physics'].mean()
+        chemistry = self.dataframe['chemistry'].mean()
+        return {'math':math, 'physics':physics, 'chemistry':chemistry}
+    
+    def min_score(self):
+        math = self.dataframe['math'].min()
+        physics = self.dataframe['physics'].min()
+        chemistry = self.dataframe['chemistry'].min()
+        return {'math':math, 'physics':physics, 'chemistry':chemistry}
+    
+    def max_score(self):
+        math = self.dataframe['math'].max()
+        physics = self.dataframe['physics'].max()
+        chemistry = self.dataframe['chemistry'].max()
+        return {'math':math, 'physics':physics, 'chemistry':chemistry}
+    
+    def get_dataframe(self):
+        return self.dataframe
